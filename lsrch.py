c1ba8c0ee3091c31bbe3ef1b154fd1e80e10ed17
Steven Appler <44624294+sappler@users.noreply.github.com>
2021-02-15 20:18:42 -0800

    comments added

--- lsrch.py
+++ lsrch.py
@@ -11,12 +11,12 @@ def main(star, en):
     if start == end:
         print("You input the same page twice :(")
         return [start]
-    if verify_search(start, end) is False:
-        print("Articles are unsearchable")  # check if searching is possible
+    if verify_search(start, end) is False: # check if either article is nonexistent
+        print("Articles are unsearchable")  
         return "Sorry, One or more of the pages you input did not exist or were impossible to navigate to."
     table = defaultdict(str)
-    table[start] = '<None>'  # dictionary of links that point to their parent
-    table[end] = '<None>'  # dictionary of links that point to their parent
+    table[start] = '<None>'  # dictionary of links that point to their parent article
+    table[end] = '<None>'  # dictionary of links that point to their parent article
     path1 = []
     path2 = []
     lock = Lock()
@@ -33,13 +33,13 @@ def main(star, en):
         print(path2)
         return path2
 
-
+#returns a HTTP parseable text stream 
 def gettext(url):
     with requests.Session() as session:
         content = session.get(url).text
     return content
 
-
+#checks if either article is "nonexistent" or is a dead-end
 def verify_search(start, end):
     nodetext1 = gettext('https://en.wikipedia.org/wiki/' + start)
     if 'Wikipedia does not have an article with this exact name' in nodetext1:
@@ -51,45 +51,45 @@ def verify_search(start, end):
         return False
     return True
 
-
+#Performs BFS from the start article
 def search(lock, table, start, path1):
     minlen = 100000
     queue = [start]
     seen = defaultdict(bool)
-    seen[start] = True
+    seen[start] = True #puts first article in dict
     while queue:
         node = queue.pop(0)
-        html = gettext('https://en.wikipedia.org/wiki/' + node)
+        html = gettext('https://en.wikipedia.org/wiki/' + node) #gets page text
         if html is None:
             continue
         parsed = HTMLParser(html)
-        div = parsed.css_first('div.mw-parser-output')
+        div = parsed.css_first('div.mw-parser-output') 
         if div is None:
             continue
-        for tag in div.css('a'):
+        for tag in div.css('a'): #for all links in the article:
             att = tag.attrs
             try:
                 href = att['href']
             except:
                 continue
             stripped = href.replace('/wiki/', '')
-            if '/wiki/' in href and ':' not in href:
+            if '/wiki/' in href and ':' not in href: #checks if the link is to another article (not list or something)
                 lock.acquire()
-                if stripped not in table:
+                if stripped not in table: #if article has not been seen by anyone, add to queue
                     table[stripped] = node
                     seen[stripped] = True
                     queue.append(stripped)
-                elif stripped not in seen:
+                elif stripped not in seen: #otherwise, if this thread hasn't seen it, but the other has, we are done.
                     key = node
                     altpath = []
-                    while key != '<None>':
+                    while key != '<None>': #traces article back to start page
                         altpath.insert(0, key)
                         key = table[key]
                     key = stripped
-                    while key != '<None>':
+                    while key != '<None>': #traces article to end page
                         altpath.append(key)
                         key = table[key]
-                    if len(altpath) < minlen:
+                    if len(altpath) < minlen: 
                         minlen = len(altpath)
                         path1.clear()
                         path1.extend(altpath)
@@ -99,7 +99,7 @@ def search(lock, table, start, path1):
             return path1
     return None
 
-
+#Performs BFS from the end article
 def reversesearch(lock, table, start, path2):
     minlen = 100000
     queue = [start]
@@ -117,7 +117,7 @@ def reversesearch(lock, table, start, path2):
         div = parsed.css_first('[id=mw-whatlinkshere-list]')
         if div is None:
             continue
-        for tag in div.css('li'):
+        for tag in div.css('li'): #for all links pointing to the article
             att = tag.css_first('a').attrs
             try:
                 href = str(att['href'])
@@ -125,18 +125,18 @@ def reversesearch(lock, table, start, path2):
                 continue
             stripped = href.replace('/wiki/', '')
             lock.acquire()
-            if stripped not in table:
+            if stripped not in table: #if article has not been seen by anyone, add to queue
                 table[stripped] = node
                 seen[stripped] = True
                 queue.append(stripped)
-            elif stripped not in seen:
+            elif stripped not in seen: #otherwise, if this thread hasn't seen it, but the other has, we are done.
                 key = node
                 altpath = []
-                while key != '<None>':
+                while key != '<None>': #traces article back to end page
                     altpath.append(key)
                     key = table[key]
                 key = stripped
-                while key != '<None>':
+                while key != '<None>': #traces article to start page
                     altpath.insert(0, key)
                     key = table[key]
                 if len(altpath) < minlen:
